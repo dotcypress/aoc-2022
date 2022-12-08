b65680a10aca9ef8b862a157544f388aae2de2cd
Vitaly Domnikov <dotcypress@gmail.com>
2022-12-08 14:00:36 -0800

    day8 cleanup

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -2,12 +2,6 @@ use crate::*;
 
 puzzle!("Day 8: Treetop Tree House", Solver, 21, 8);
 
-pub enum Dir {
-    Up,
-    Right,
-    Down,
-    Left,
-}
 struct Solver {
     map: Vec<Vec<usize>>,
 }
@@ -34,37 +28,32 @@ impl Solver {
             .unwrap_or_default()
     }
 
-    fn scenic_score(&self, x: usize, y: usize) -> usize {
-        let height = self.map[y][x];
-        let init = (false, 0);
-        let kernel = |(blocked, cnt), t: &usize| {
-            if blocked {
-                (blocked, cnt)
-            } else if *t >= height {
-                (true, cnt + 1)
-            } else {
-                (false, cnt + 1)
-            }
-        };
-
-        let u = self.look_from(x, y, Dir::Up).iter().fold(init, kernel);
-        let r = self.look_from(x, y, Dir::Right).iter().fold(init, kernel);
-        let d = self.look_from(x, y, Dir::Down).iter().fold(init, kernel);
-        let l = self.look_from(x, y, Dir::Left).iter().fold(init, kernel);
-
-        u.1 * r.1 * d.1 * l.1
-    }
-
     fn is_visible_from_edges(&self, x: usize, y: usize) -> bool {
         let height = self.map[y][x];
-        let pred = |h: &&usize| **h >= height;
+        self.look_around(x, y)
+            .iter()
+            .map(|view| view.iter().filter(|h: &&usize| **h >= height).count())
+            .any(|blockers| blockers == 0)
+    }
 
-        let u = self.look_from(x, y, Dir::Up).iter().filter(pred).count();
-        let r = self.look_from(x, y, Dir::Right).iter().filter(pred).count();
-        let d = self.look_from(x, y, Dir::Down).iter().filter(pred).count();
-        let l = self.look_from(x, y, Dir::Left).iter().filter(pred).count();
-
-        u == 0 || r == 0 || d == 0 || l == 0
+    fn scenic_score(&self, x: usize, y: usize) -> usize {
+        let height = self.map[y][x];
+        self.look_around(x, y)
+            .iter()
+            .map(|view| {
+                view.iter()
+                    .fold((false, 0), |(blocked, cnt), t: &usize| {
+                        if blocked {
+                            (blocked, cnt)
+                        } else if *t >= height {
+                            (true, cnt + 1)
+                        } else {
+                            (false, cnt + 1)
+                        }
+                    })
+                    .1
+            })
+            .product()
     }
 
     fn points(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
@@ -74,12 +63,12 @@ impl Solver {
             .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, _)| (x, y)))
     }
 
-    fn look_from(&self, x: usize, y: usize, dir: Dir) -> Vec<usize> {
-        match dir {
-            Dir::Up => self.map.iter().take(y).map(|c| c[x]).rev().collect(),
-            Dir::Right => self.map[y].iter().skip(x + 1).copied().collect(),
-            Dir::Down => self.map.iter().skip(y + 1).map(|c| c[x]).collect(),
-            Dir::Left => self.map[y].iter().take(x).rev().copied().collect(),
-        }
+    fn look_around(&self, x: usize, y: usize) -> Vec<Vec<usize>> {
+        vec![
+            self.map.iter().take(y).map(|c| c[x]).rev().collect(),
+            self.map[y].iter().skip(x + 1).copied().collect(),
+            self.map.iter().skip(y + 1).map(|c| c[x]).collect(),
+            self.map[y].iter().take(x).rev().copied().collect(),
+        ]
     }
 }
