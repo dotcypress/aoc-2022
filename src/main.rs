87e219d07ed251cfc7141f1e86e0ebbd89131051
Vitaly Domnikov <dotcypress@gmail.com>
2022-12-10 15:26:03 -0800

    uppdate puzzle runner

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use aoc_2022::*;
+use std::env;
 
 mod day1;
 mod day10;
@@ -25,15 +26,34 @@ pub const PUZZLES: [Puzzle; 10] = [
 ];
 
 fn main() {
-    println!("╔══════════════════════════════════╦════════════════╦════════════════╗");
-    println!("║ 🦀 Advent of Code 2022           ║       Part One ║       Part Two ║");
-    println!("╠══════════════════════════════════╬════════════════╬════════════════╣");
-    for puzzle in PUZZLES.iter().rev() {
-        let name = puzzle.name;
-        let (mut part_one, mut part_two) = (puzzle.solve)();
-        part_one.truncate(14);
-        part_two.truncate(14);
-        println!("║ {: <32} ║ {: >14} ║ {: >14} ║", name, part_one, part_two);
+    if let Some(day) = env::args().skip(1).next() {
+        let day = day
+            .chars()
+            .filter(|c| c.is_numeric())
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap_or_default();
+        if day <= PUZZLES.len() {
+            let puzzle = &PUZZLES[day - 1];
+            let name = puzzle.name;
+            let (part_one, part_two) = (puzzle.solve)();
+            println!("{name}");
+            println!("\nPart One:\n{part_one}");
+            println!("\nPart Two:\n{part_two}");
+        } else {
+            println!("Day {day} puzzle not found");
+        }
+    } else {
+        println!("╔══════════════════════════════════╦════════════════╦════════════════╗");
+        println!("║ 🦀 Advent of Code 2022           ║       Part One ║       Part Two ║");
+        println!("╠══════════════════════════════════╬════════════════╬════════════════╣");
+        for puzzle in PUZZLES.iter().rev() {
+            let name = puzzle.name;
+            let (mut part_one, mut part_two) = (puzzle.solve)();
+            part_one.truncate(14);
+            part_two.truncate(14);
+            println!("║ {: <32} ║ {: >14} ║ {: >14} ║", name, part_one, part_two);
+        }
+        println!("╚══════════════════════════════════╩════════════════╩════════════════╝");
     }
-    println!("╚══════════════════════════════════╩════════════════╩════════════════╝");
 }
